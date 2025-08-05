6a3c77cee4f84954ad4e668fc4292cad01de2956
zhutaoyu <zhutaoyu97@gmail.com>
2025-08-05 13:59:47 +0800

    change 30_batched_matrix_multiplication rtol & atol from 1e-5 to 1e-3

--- challenges/medium/30_batched_matrix_multiplication/challenge.py
+++ challenges/medium/30_batched_matrix_multiplication/challenge.py
@@ -7,8 +7,8 @@ class Challenge(ChallengeBase):
     def __init__(self):
         super().__init__(
             name="Batched Matrix Multiplication",
-            atol=1e-5,
-            rtol=1e-5,
+            atol=1e-3,
+            rtol=1e-3,
             num_gpus=1,
             access_tier="free"
         )
